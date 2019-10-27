4bea9f72bb7119aadb0a6971400d8a18e1bc7e69
William <ertyjunior@gmail.com>
2019-10-27 07:50:07 -0500

    gave it the jazz hands treatment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import logo from "./images/donut.jpeg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Step right up! Read all about it!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-background">
+        <div className="container">
+          <h1>Step right up! Read all about it!</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
       </header>
     </div>
   );
